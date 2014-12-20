c911c7ae1e17a0aecfc2a6179b46c6d5cc8cc69d
scott <scott.lan@taisys.com>
2014-12-21 03:39:53 +0800

    update authorization #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,34 +7,34 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-  def auto_detect_user_loggined
-    @loggined_user_id = session[:logined_user_id]
-  end
-
-  def auto_remove_expired_cookie
-    puts session[:logined_user_id]
-  end
-
-  def check_user_loggedin
-    if session[:logined_user_id] == nil
-      render nothing: true, status: 401
-      return
+    def auto_detect_user_loggined
+      @loggined_user_id = session[:logined_user_id]
     end
 
-    begin
-      User.find(session[:logined_user_id])
-    rescue
-      render nothing: true, status: 401
+    def auto_remove_expired_cookie
+      puts session[:logined_user_id]
     end
-  end
 
-  def check_editable(obj)
-    if obj.created_user != @loggined_user_id
-      render nothing: true, status: 401
+    def check_user_loggedin
+      if session[:logined_user_id] == nil
+        render nothing: true, status: 401
+        return
+      end
+
+      begin
+        User.find(session[:logined_user_id])
+      rescue
+        render nothing: true, status: 401
+      end
     end
-  end
 
-  def check_visiable(obj)
-    obj.created_user == @loggined_user_id
-  end
+    def check_editable(obj)
+      unless check_visiable(obj)
+        render nothing: true, status: 401
+      end
+    end
+
+    def check_visiable(obj)
+      obj.created_user == @loggined_user_id
+    end
 end
